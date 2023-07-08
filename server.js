9cd8f1ca27343164abb0748e4179b85f6e735b4c
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-08 14:42:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const sequelize = require('./util/database');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(3333);
+const adminRoutes = require('./routes/admin');
+
+app.use(adminRoutes);
+
+sequelize
+    .sync()
+    //.sync({force:true})
+    .then(result => {
+        app.listen(3333);
+    })
+    .catch(err => console.log(err))
