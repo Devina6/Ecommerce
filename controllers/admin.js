0f28226f67c1b101b488e5ddad098ac8993c7e8e
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-08 12:45:50 +0530

    Create admin.js

---
+++ controllers/admin.js
@@ -0,0 +1,13 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req,res,next) =>{
+
+}
+
+exports.addProduct = (req,res,next) => {
+
+}
+
+exports.deleteProduct = (req,res,next) => {
+    
+}
